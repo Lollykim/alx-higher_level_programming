b021d538885195b256fe655a8bdb5564cb3c0eb7
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-02 23:21:57 +0000

    My task

---
+++ 0x02-python-import_modules/1-calculation.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if _name_ == "_main_"
+from calculator_1 import add
+a = 10
+b = 5
+print("{:d} + {:d} + {:d}".format(a, b, add(a, b)))
+
+if _name_ == "_main_"
+form calculator_1 import subtract
+a = 10
+b = 5
+print("{d} - {:d} = {:d}". format(a, b, subtract(a,b)))
+
+from calculator_1 import multiply
+a = 10
+b = 5
+print("{:d} * {:d} = {:d}".format(a, b, multiply(a,b)))
+
+from calculator_1 import divide
+a = 10
+b = 5
+print("{:d} / {:d} = {:d}".format(a, b, divide(a, b)))
