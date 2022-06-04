f6359f6e2e8cc7548e7e3a1e0e2daf67e086eb9d
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-04 21:22:31 +0000

    My task commit

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -4,22 +4,24 @@ if __name__ == "__main__":
    from sys import argv
    num = len(argv)
 
-   num != 4:
-      print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-      exit(1)
-   if operator != '+' and operator != '-' and operator != '*' and operator != '/'
-      print("Unknown operator. Available operators: +, -, * and /")
-      exit(1)
+   num != 4
+     
+   op != '+' and op != '-' and op != '*' and op != '/'
 
    a = int(argv[1])
    operator = argv[2]
    b = int(argv[3]
 
-   if operator == '+':
-      print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
-   elif operator == '-':
-      print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
-   elif operator == '*':
-      print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
-   else operator == '/':
-      printt("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+   if operator == '+'
+       print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+   elif operator == '-'
+       print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+   elif operator == '*'
+       print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+   else operator == '/'
+       print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
+
+       print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+         exit(1)
+       print("Unknown operator. Available operators: +, -, * and /")
+         exit(1)
