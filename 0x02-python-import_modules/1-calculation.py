fa3ad17b2adecb3705949b25fe0f67d33a9d8d6d
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-04 18:35:20 +0000

    task commit

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
-if _name_ == "_main_"
-from calculator_1 import add
-a = 10
-b = 5
-print("{:d} + {:d} + {:d}".format(a, b, add(a, b)))
+if __name__ == "__main__"
+   from calculator_1 import add
+   a = 10
+   b = 5
+   print("{:d} + {:d} + {:d}".format(a, b, add(a, b)))
 
-if _name_ == "_main_"
-form calculator_1 import subtract
-a = 10
-b = 5
-print("{d} - {:d} = {:d}". format(a, b, subtract(a,b)))
+if __name__ == "__main__"
+   from calculator_1 import sub
+   a = 10
+   b = 5
+   print("{d} - {:d} = {:d}". format(a, b, subtract(a,b)))
 
-from calculator_1 import multiply
-a = 10
-b = 5
-print("{:d} * {:d} = {:d}".format(a, b, multiply(a,b)))
+if __name__ == "__main__"
+   from calculator_1 import mul
+   a = 10
+   b = 5
+   print("{:d} * {:d} = {:d}".format(a, b, mul(a,b)))
 
-from calculator_1 import divide
-a = 10
-b = 5
-print("{:d} / {:d} = {:d}".format(a, b, divide(a, b)))
+if __name__ == "__main__"
+   from calculator_1 import div
+   a = 10
+   b = 5
+   print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
