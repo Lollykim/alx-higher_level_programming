474a8d56f5cff3a00802a89c13c1b16db818a2ac
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-06 08:35:48 +0000

    tast done

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+cp = a[:]
+for i in range(len(my_list)):
+
+      if idx < 0:
+     return a[:]
+
+if idx >= len(my_list):
+return a[:]
+else:
+a[:][idx] = element
+return a[:]
