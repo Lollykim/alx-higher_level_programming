9b3ec3324b3920cf72ac6eba1805eae125bf801d
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-20 23:02:49 +0000

    Addedfiles: safe print integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -4,6 +4,7 @@
 def safe_print_integer(value):
     try:
  	print("{:d}".format(value))
-	return(True)
-    except (ValueError, TypeError):
-        return(False)
+    except:
+	return (False)
+    else:
+        return (True)
