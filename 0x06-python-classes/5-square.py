74c496e09e7553fcdb295df5119a3ef197709971
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-22 23:07:56 +0000

    advance task added

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,33 +1,55 @@
 #!/usr/bin/python3
+""" square class
+"""
 
 
 class Square:
-    """Class to represent a square."""
+    """ Empty class with size private attribute
+    """
+    def __init__(self, size=0):
+        """
+                Instantiation with size
+        Args:
+            size: size of the square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
 
-    def __init__(self, size):
-        """Initialize a new square."""
-        self.size = size
-
-    def aread(self):
-        return (seld=f.__size ** 2)
+    def area(self):
+        """
+        Returns the area of the square
+        """
+        return (self.__size * self.__size)
 
     @property
     def size(self):
-        """Get/set the current size of the square."""
-        return (self.__size)
+        """
+        size getter. Handle size errors
+        """
+        return self.__size
 
     @size.setter
     def size(self, value):
-        if not isinstance(value, int):
+        """
+        size setter. Set the size square
+        """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
     def my_print(self):
-        """Print the square with the # character."""
-        for i in range(0, self.__size):
-            [print("#", end="") for j in range(self.__size)]
-            print("")
+        """
+        Print a square with the character #
+        """
         if self.__size == 0:
-            print("")
+            print()
+        else:
+            for row in range(self.__size):
+                for column in range(self.__size):
+                    print("#", end="")
+                print()
