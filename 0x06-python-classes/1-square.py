bd09e818b61cd11d0cbca69a06ac4df112fd750a
Olorunfemi Omolola <lollyoluwafemi545@gmail.com>
2022-06-22 22:44:50 +0000

    added file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""Square module."""
 
 
 class Square:
-    """Class defining a square"""
+    """Defines a square."""
+
     def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of a side of the square.
+        """
         self.__size = size
